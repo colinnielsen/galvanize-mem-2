04f860706995b64a75a82f40c6541fd830794cc7
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2018-05-22 13:51:25 -0600

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,37 +6,8 @@ module.exports = {
       client: "pg",
       connection: "postgres://localhost:/drill-2"
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+        client: "pg",
+        connection: process.env.DATABASE_URL
     }
-  }
-
 };
